aeaff18341ad519bdda150691018f777fca19cfe
marss19_cp <marss19@users.noreply.github.com>
2014-11-21 12:38:56 +0000

    added ability to compare formatted JSON results incremeted version to 1.0.2

---
+++ trunk/JsonViewer/Forms/HtmlBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marss.JsonViewer.Forms
+{
+    public static class HtmlBuilder
+    {
+        public static string PrepateHtml(string json, bool keepJsonMarkup)
+        {
+            json = json.Replace(Environment.NewLine, "").Replace("\\", "\\\\").Replace("\"", "\\\"");
+            var html = ReadContentFromResources("Marss.JsonViewer.Resources.JsonView.htm");
+            html = html.Replace("[InputDataPlaceholder]", json);
+
+            return html.Replace("[KeepJsonMarkupPlaceholder]", keepJsonMarkup.ToString().ToLower());
+        }
+
+        #region private
+
+        private static string ReadContentFromResources(string pathToResource)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(pathToResource))
+            {
+                if (stream != null)
+                {
+                    return new StreamReader(stream).ReadToEnd();
+                }
+            }
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/JsonViewer/Forms/MainForm.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Marss.JsonViewer.Forms
+{
+    /// <summary>
+    /// Interaction logic for MyControl.xaml
+    /// </summary>
+    public partial class MainForm : UserControl
+    {
+        public MyToolWindow _parent;
+
+        public MainForm(MyToolWindow parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+        }
+
+        private void FormatJson(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbData.Text))
+                return;
+
+            var html = HtmlBuilder.PrepateHtml(tbData.Text, btnProgrammerFriendlyView.IsChecked.HasValue && btnProgrammerFriendlyView.IsChecked.Value);
+            webBrowser.NavigateToString(html);
+        }
+
+        private void PrintFormattedJson(object sender, RoutedEventArgs e)
+        {
+            var doc = webBrowser.Document as mshtml.IHTMLDocument2;
+            if (doc != null)
+                doc.execCommand("Print", true, null);
+        }
+
+        private void CompareToAnotherJson(object sender, RoutedEventArgs e)
+        {
+            var comparisonWindow = new ComparisonWindow();
+            comparisonWindow.SetInitialData((IServiceProvider)_parent.Package, tbData.Text);
+            comparisonWindow.ShowDialog();
+        }
+        
+    }
+}
