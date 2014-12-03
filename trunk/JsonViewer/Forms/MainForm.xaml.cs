085f48b9e87d26b1580c2cf8f2b9a0b8e55cc960
marss19_cp <SND\marss19_cp>
2014-12-03 17:24:02 +0000

    added ability to change layout

--- trunk/JsonViewer/Forms/MainForm.xaml.cs
+++ trunk/JsonViewer/Forms/MainForm.xaml.cs
@@ -28,6 +28,8 @@ namespace Marss.JsonViewer.Forms
         {
             InitializeComponent();
             _parent = parent;
+
+            tbData.Focus();
         }
 
         private void FormatJson(object sender, RoutedEventArgs e)
@@ -52,6 +54,54 @@ namespace Marss.JsonViewer.Forms
             comparisonWindow.SetInitialData((IServiceProvider)_parent.Package, tbData.Text);
             comparisonWindow.ShowDialog();
         }
-        
+
+        private void SplitHorizontal(object sender, RoutedEventArgs e)
+        {
+            grid.ColumnDefinitions[0].Width = new GridLength(1, GridUnitType.Star);
+            grid.ColumnDefinitions[1].Width = new GridLength(0);
+
+            grid.RowDefinitions[0].Height = new GridLength(200);
+            grid.RowDefinitions[1].Height = new GridLength(1, GridUnitType.Star);
+
+            Grid.SetColumn(pnlFormattedView, 0);
+            Grid.SetRow(pnlFormattedView, 1);
+            pnlFormattedView.Margin = new Thickness(5); 
+
+            splitter.ResizeDirection = GridResizeDirection.Rows;
+            splitter.Height = 5;
+            splitter.Width = Double.NaN;
+            splitter.HorizontalAlignment = HorizontalAlignment.Stretch;
+            splitter.VerticalAlignment =  VerticalAlignment.Bottom;
+            splitter.Margin = new Thickness(5, 0, 5, 0);
+            splitter.Cursor = Cursors.SizeNS;
+
+            Grid.SetColumnSpan(splitter, 2);
+            Grid.SetRowSpan(splitter, 1);
+        }
+
+        private void SplitVertical(object sender, RoutedEventArgs e)
+        {
+            grid.ColumnDefinitions[0].Width = new GridLength(200);
+            grid.ColumnDefinitions[1].Width = new GridLength(1, GridUnitType.Star);
+
+            grid.RowDefinitions[0].Height = new GridLength(1, GridUnitType.Star);
+            grid.RowDefinitions[1].Height = new GridLength(0);
+            
+            Grid.SetColumn(pnlFormattedView, 1);
+            Grid.SetRow(pnlFormattedView, 0);
+            pnlFormattedView.Margin = new Thickness(0, 5, 5, 5); 
+
+            splitter.ResizeDirection = GridResizeDirection.Columns;
+            splitter.Height = Double.NaN;
+            splitter.Width = 5;
+            splitter.HorizontalAlignment = HorizontalAlignment.Right;
+            splitter.VerticalAlignment = VerticalAlignment.Stretch;
+            splitter.Margin = new Thickness(0, 5, 0, 5);
+            splitter.Cursor = Cursors.SizeWE;
+
+            Grid.SetColumnSpan(splitter, 1);
+            Grid.SetRowSpan(splitter, 2);
+        }
+
     }
 }
